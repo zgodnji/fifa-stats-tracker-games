8efd90bf8f66e3552f1c9bb94b6d908071e3eef9
Jakob Salej <jakob.salej@gmail.com>
2017-12-10 17:18:45 +0100

    Etcd service discovery not working

--- src/main/java/com/zgodnji/fifastatstracker/GameApplication.java
+++ src/main/java/com/zgodnji/fifastatstracker/GameApplication.java
@@ -1,9 +1,12 @@
 package com.zgodnji.fifastatstracker;
 
+import com.kumuluz.ee.discovery.annotations.RegisterService;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
 
+@RegisterService
 @ApplicationPath("v1")
 public class GameApplication extends Application {
 }
